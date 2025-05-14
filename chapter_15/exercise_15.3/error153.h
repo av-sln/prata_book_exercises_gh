890ba25597d949d3bc3eee171ee19b7b8f22154d
avsln <mail@salnikov-pro.ru>
2025-05-14 21:02:11 +0300

    Added error153.h and use_error153.cpp

---
+++ chapter_15/exercise_15.3/error153.h
@@ -0,0 +1,44 @@
+// error153.h -- заголовочный файла исключений logic_error
+#ifndef ERROR153_H_
+#define ERROR153_H_
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+class bad_hmean : public std::invalid_argument {
+private:
+  double arg1;
+  double arg2;
+public:
+  explicit bad_hmean(const std::string& what_arg, double a = 0.0, 
+    double b = 0.0)
+    : std::invalid_argument(what_arg), arg1(a), arg2(b) {}
+  void show() const;
+};
+
+class bad_gmean : public std::domain_error {
+private:
+  double arg1;
+  double arg2;
+public:
+  explicit bad_gmean(const std::string& what_arg, double a = 0.0, 
+    double b = 0.0)
+    : std::domain_error(what_arg), arg1(a), arg2(b) {}
+  void show() const;
+};
+
+// Встроенный метод bad_hmean
+inline void bad_hmean::show() const
+{
+  std::cout << "Funtion is hmean(): argument 1 = " << arg1 << ", "
+            << "argument 2 = " << arg2 << std::endl;
+}
+
+// Встроенный метод bad_gmean
+inline void bad_gmean::show() const
+{
+  std::cout << "Function is gmean(): argument 1 = " << arg1 << ", "
+            << "argument 2 = " << arg2 << std::endl;
+}
+
+#endif  // error153.h
---
+++ chapter_15/exercise_15.3/use_error153.cpp
@@ -0,0 +1,59 @@
+// use_error153.cpp -- использование класса исключений logic_error
+#include <iostream>
+#include <cmath>
+#include <exception>
+#include "error153.h"
+
+// Протитпы функций
+double hmean(double a, double b);
+double gmean(double a, double b);
+
+int main()
+{
+  double x, y, z;
+  std::cout << std::endl;
+  std::cout << "Enter two numbers: ";
+  while (std::cin >> x >> y) {
+    try {
+      z = hmean(x, y);
+      std::cout << "Harmonic mean of " << x << " and " << y 
+                << " is " << z << std::endl;
+      z = gmean(x, y);
+      std::cout << "Geometric mean of " << x << " and " << y
+                << " is " << z << std::endl;
+      std::cout << "Enter next set of numbers <q to quit>: ";
+    }
+    catch (std::logic_error& ex) {
+      if (typeid(bad_hmean) == typeid(ex)) {
+        bad_hmean& temp = dynamic_cast<bad_hmean&>(ex);
+        temp.show();
+        std::cout << temp.what() << std::endl;
+        std::cout << "Sorry, you don't get to play any more.\n";
+        break;
+      } else if (typeid(bad_gmean) == typeid(ex)) {
+          bad_gmean& temp = dynamic_cast<bad_gmean&>(ex);
+          temp.show();
+          std::cout << temp.what() << std::endl;
+          std::cout << "Sorry, you don't get to play any more.\n";
+          break;
+      }
+    }
+  }
+  std::cout << "Bye!\n";
+
+  return 0;
+}
+
+double hmean(double a, double b)
+{
+  if (a == -b)
+    throw bad_hmean("Error is in hmean. Invalid arguments a = -b.", a, b);
+  return 2.0 * a * b / (a + b);
+}
+
+double gmean(double a, double b)
+{
+  if (a < 0 || b < 0)
+    throw bad_gmean("Error is in gmean(). Arguments should be >= 0.", a, b);
+  return std::sqrt(a * b);
+}
